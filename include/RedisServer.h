10c2d6c59dd6a9b514d7f90f4c31cb3e7b7ca10c
Eliezer Then <eliezerthen080@gmail.com>
2026-01-10 04:06:20 -0500

    Created RedisServer class

---
+++ include/RedisServer.h
@@ -0,0 +1,17 @@
+#ifndef REDIS_SERVER_H
+#define REDIS_SERVER_H
+
+class RedisServer {
+    public:
+    RedisServer(int port);
+
+    private:
+    int port;
+};
+
+
+
+
+
+
+#endif // REDIS_SERVER_H
